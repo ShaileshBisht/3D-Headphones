4c50664f5a32fa94f8a273691b61a172ede470e6
shaileh Bisht <shaileshbisht765@gmail.com>
2021-06-29 23:57:42 +0530

    add headphone model

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Scene from "./Scene";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="canvasFrame">
+        <Canvas>
+          <ambientLight intensity={0.3} />
+          <pointLight position={[10, 10, 10]} />
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </div>
     </div>
   );
 }
